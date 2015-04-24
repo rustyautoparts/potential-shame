7483b3e34bbf25f08b9233cef3eacbfcb539696c
Russ Otto <unplugged68@gmail.com>
2015-04-24 12:48:24 -0500

    cleaned up nodeschool folder

--- nodeschool/asyn.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-var file = process.argv[2];
-fs.readFile(file, function(err, data) {
-	if (err) throw err;
-	var content = data.toString();
-	var lines = content.split('\n').length - 1;
-	console.log(lines);
-});
