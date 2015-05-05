4ae2faef922be5572ac82488f912f38b712c9a92
Russ Otto <unplugged68@gmail.com>
2015-05-05 09:28:34 -0500

    finished static.js and jade.js exercises

---
+++ nodeschool/expressworks/static.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+var port = process.argv[2];
+
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
+app.listen(port);
