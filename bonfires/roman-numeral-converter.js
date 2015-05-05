54f883c39c2f5297e898c4ed2fdb2420d4e6fed6
Russ Otto <unplugged68@gmail.com>
2015-05-05 16:40:13 -0500

    started roman-numeral-converter.js

---
+++ bonfires/roman-numeral-converter.js
@@ -0,0 +1,21 @@
+// Roman Numeral Converter
+//
+// Convert the number to be a roman numeral
+// All roman numerals answers should be provided in upper-case
+
+function convert(num) {
+	return num;
+}
+
+convert(36);
+// >> XXXVI
+convert(12);
+// >> XXI
+convert(5);
+// >> V
+convert(9);
+// >> IX
+convert(29);
+// >> XXIX
+convert(16);
+// >> XVI
