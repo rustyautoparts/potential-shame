74d5f3c10e7aef477576b2acef4c7f8a354b41d1
Russ Otto <unplugged68@gmail.com>
2015-05-10 12:34:40 -0500

    solved everything-be-true.js

--- bonfires/everything-be-true.js
+++ bonfires/everything-be-true.js
@@ -5,10 +5,26 @@
 //Remember, you can access object properties through either dot notation or [] notation.
 
 function every(collection, pre) {
-	collection.forEach(function(val) {
-		
+	if (typeof pre == 'string') {
+		for (var i = 0; i < collection.length; i++) {
+			if (!collection[i].hasOwnProperty(pre)) {
+				return false;
+			}
+		}
+	} else if (typeof pre == 'object') {
+		var prop = Object.getOwnPropertyNames(pre).join('');
+		for (var i = 0; i < collection.length; i++) {
+			if (pre[prop] != collection[i][prop]) {
+				return false;
+			}
+		}
+	} else {
+		console.log('Error: second argument must be object or string');
+		return false;
 	}
-	return pre;
+	return true;
 }
 
 every([{'user': 'Tinky-Winky', 'sex': 'male'}, {'user': 'Dipsy', 'sex': 'male'}, {'user': 'Laa-Laa', 'sex': 'female'}, {'user': 'Po', 'sex': 'female'}], 'sex');
+every([{'user': 'Tinky-Winky', 'sex': 'male'}, {'user': 'Dipsy', 'sex': 'male'}, {'user': 'Laa-Laa', 'sex': 'female'}, {'user': 'Po', 'sex': 'female'}], 'ballons');
+every([{'user': 'Tinky-Winky', 'sex': 'male'}, {'user': 'Dipsy', 'sex': 'male'}, {'user': 'Laa-Laa', 'sex': 'female'}, {'user': 'Po', 'sex': 'female'}], {'sex': 'male'});
