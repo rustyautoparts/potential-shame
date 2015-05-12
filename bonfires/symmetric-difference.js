b81fd23177dda45525e40ebb3a143428d3c98208
Russ Otto <unplugged68@gmail.com>
2015-05-12 16:28:15 -0500

    init symmetric-difference.js

---
+++ bonfires/symmetric-difference.js
@@ -0,0 +1,10 @@
+// Symmetric Difference
+//
+// Create a function that takes two or more arrays and returns an array of the symmetric difference of the provided arrays.
+// The mathematical term symmetric difference refers to the elements in two sets that are in either the first or second set, but not in both.
+
+function sym(args) {
+	return args;
+}
+
+sym([1, 2, 3], [5, 2, 1, 4]);
