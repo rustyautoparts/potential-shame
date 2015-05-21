5af7742bec1680741f72f3b60a80b812fb55e73d
Russ Otto <unplugged68@gmail.com>
2015-05-21 12:53:33 -0500

    finished hello world, init bob

--- exercism/javascript/hello-world/hello-world.js
+++ exercism/javascript/hello-world/hello-world.js
@@ -1,14 +1,10 @@
-//
-// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 var HelloWorld = function() {};
 
 HelloWorld.prototype.hello = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+	if (!input) {
+		input = "world";
+	}
+	return "Hello, " + input + "!"
 };
 
 module.exports = HelloWorld;
