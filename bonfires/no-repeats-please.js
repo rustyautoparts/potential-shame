0e33d7298f9d50c57749ea0d7f7dda7b2c7897e5
Russ Otto <unplugged68@gmail.com>
2015-05-27 16:38:59 -0500

    completed week 1 of mongodb course

--- bonfires/no-repeats-please.js
+++ bonfires/no-repeats-please.js
@@ -4,23 +4,10 @@
 // For example, 'aab' should return 2 because it has 6 total permutations, but only 2 of them don't have the same letter (in this case 'a') repeating.
 
 function permAlone(str) {
-	var array = str.split('');
-	var result = [];
-	function perm(arr, opts) {
-		var holder = [];
-		for(var i = 0; i < arr.length; i++) {
-			holder.concat(arr.splice(i, 1));
-		}
-		return holder;
-	}
-	return perm(array);
-}
-function notSame(letter1, letter2) {
-	if (letter1 === letter2) {
-		return false;
-	}
-	return true;
+  var arr = str.split('');
+  var count = 0;
 
+  return count;
 }
 
 console.log(permAlone('aab'));
