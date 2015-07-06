a439b39a0433d26f81b7f629fe6844b655319ed2
Russ Otto <unplugged68@gmail.com>
2015-07-06 12:16:21 -0500

    fixed error using git to move bonfires into new directory

--- bonfires/spinal-case.js
+++
@@ -1,6 +0,0 @@
-function spinalCase(str) {
-	str = str.replace(/([a-z])([A-Z])/g, '$1 $2')
-		 .toLowerCase()
-		 .replace(/(\s|_)+/g, '-');
-	return str;
-}
--- bonfires/symmetric-difference_2.js
+++
@@ -1,8 +0,0 @@
-function sym(args) {
-	var argArr = [];
-	for (var i = 0; i < arguments.length; i++) {
-		argArr.push(arguments[i]);
-	}
-}
-
-console.log(sym([1,2,4], [5,2,1,3], [1,2,8,7]));
