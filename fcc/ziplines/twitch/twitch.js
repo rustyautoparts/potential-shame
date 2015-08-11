11caeb9817f59b16b6c2c81e7bc954c614838908
Russ Otto <unplugged68@gmail.com>
2015-08-11 11:05:09 -0500

    Added styling and some JS refactoring

--- fcc/ziplines/twitch/twitch.js
+++ fcc/ziplines/twitch/twitch.js
@@ -1,21 +1,30 @@
 $(document).ready(function() {
   var streams = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","comster404","brunofin","thomasballinger","noobs2ninjas","medrybw","beohoff"]
   streams.forEach(function(stream) {
-    $("#streams").append("<li id='" + stream + "' class='stream'></li>");
-    $.getJSON("https://api.twitch.tv/kraken/channels/" + stream + "?callback=?", function(data) {
-    }).done(function(data) {
+    $.getJSON("https://api.twitch.tv/kraken/channels/" + stream + "?callback=?")
+      .done(function(data) {
       var name = data.display_name;
       var avatar = data.logo;
-      $("#" + stream).append("<img class='avatar' src='" + avatar + "'></div>");
-      $("#" + stream).append("<h3 class='name'>" + name + "</h3>");
-    });
-    $.getJSON( "https://api.twitch.tv/kraken/streams/" + stream + "?callback=?", function(data) {
-    }).done(function(data) {
-      if (!data.stream) {
-        $("#" + stream).append("<p>Offline</p>");
+      var streamItem = "#" + stream;
+      if (data.error) {
+        console.log("ERROR: " + stream + " " + data.error);
       } else {
-        $("#" + stream).append("<p>Online</p>");
-        $("#" + stream).append("<p>" + data.stream.channel.status + "</p>");
+        $("#streams").append("<li id='" + stream + "' class='stream'></li>");
+        if (!avatar) {
+          avatar = "404user.png";
+        }
+        $(streamItem).append("<img class='avatar' src='" + avatar + "'></div>");
+        $(streamItem).append("<h3 class='name'>" + name + "</h3>");
+        $(streamItem).append("<p class='status'></p>");
+        $.getJSON( "https://api.twitch.tv/kraken/streams/" + stream + "?callback=?")
+          .done(function(data) {
+          if (!data.stream) {
+            $(streamItem + " .status").append("Offline");
+          } else {
+            $(streamItem + " .status").append(data.stream.channel.status);
+          }
+          $(streamItem).children().wrapAll("<a href='" + data.url + "'></a>");
+        });
       }
     });
   });
