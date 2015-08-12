ac19a462b0247bf92d6d94c6163590255b48e2a1
Russ <unplugged68@gmail.com>
2015-08-11 22:20:23 -0500

    Messing around with the jquery template plugin

--- fcc/ziplines/twitch/twitch.js
+++ fcc/ziplines/twitch/twitch.js
@@ -1,29 +1,28 @@
 $(document).ready(function() {
   var streams = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","comster404","brunofin","thomasballinger","noobs2ninjas","medrybw","beohoff"]
   streams.forEach(function(stream) {
+    var template = {};
     $.getJSON("https://api.twitch.tv/kraken/channels/" + stream + "?callback=?")
-      .done(function(data) {
-      var name = data.display_name;
-      var avatar = data.logo;
-      var streamItem = "#" + stream;
-      if (data.error) {
-        console.log("ERROR: " + stream + " " + data.error);
+     .done(function(channel) {
+      if (channel.error) {
+        console.log("ERROR: " + channel + " " + channel.error);
       } else {
-        $("#streams").append("<li id='" + stream + "' class='stream'></li>");
-        if (!avatar) {
-          avatar = "404user.png";
+        template.channel = stream;
+        template.name = channel.display_name;
+        template.url = channel.url;
+        if (!channel.logo) {
+          template.avatar = "404user.png";
+        } else {
+          template.avatar = channel.logo;
         }
-        $(streamItem).append("<img class='avatar' src='" + avatar + "'></div>");
-        $(streamItem).append("<h3 class='name'>" + name + "</h3>");
-        $(streamItem).append("<p class='status'></p>");
         $.getJSON( "https://api.twitch.tv/kraken/streams/" + stream + "?callback=?")
-          .done(function(data) {
+         .done(function(data) {
           if (!data.stream) {
-            $(streamItem + " .status").append("Offline");
+            template.desc = "Offline";
           } else {
-            $(streamItem + " .status").append(data.stream.channel.status);
+            template.desc = data.stream.channel.status;
           }
-          $(streamItem).children().wrapAll("<a href='" + data.url + "'></a>");
+          $("#streams").loadTemplate($("template"), template);
         });
       }
     });
