161228ca73a99fb95035aae29a5caa90695e3a62
Russ Otto <unplugged68@gmail.com>
2015-09-17 14:28:58 -0500

    List randomizer is work in progress

--- fcc/ziplines/quotes/quotes-list.js
+++ fcc/ziplines/quotes/quotes-list.js
@@ -5,29 +5,9 @@ var quotes = [
     "source": "An interview by Peter Seibel in <em>Coders At Work</em>"
   },
   {
-    "quote": "On two occasions I have been asked, &#x2013; \"Pray, Mr. Babbage, if you put into the machine wrong figures, will the right answers come out?\" In one case a member of the Upper, and in the other a member of the Lower House put this question. I am not able rightly to apprehend the kind of confusion of ideas that could provoke such a question.",
-    "author": "Charles Babbage",
-    "source": "<em>Passages from the Life of a Philosopher</em>"
-  },
-  {
-    "quote": "Playing with pointers is like playing with fire. Fire is perhaps the most important tool known to man. Carefully used, fire brings enormous benefits; but when fire gets out of control, disaster strikes.",
-    "author": "John Barnes",
-    "source": "<em>Programming in Ada 2012</em>"
-  },
-  {
-    "quote": "The programmer, like the poet, works only slightly removed from pure thought-stuff. He builds his castles in the air, from air, creating by exertion of the imagination. Few media of creation are so flexible, so easy to polish and rework, so readily capable of realizing grand conceptual structures&#x2026 Yet the program construct, unlike the poet's words, is real in the sense that it moves and works, producing visible outputs separate from the construct itself. [&#x2026] The magic of myth and legend has come true in our time. One types the correct incantation on a keyboard, and a display screen comes to life, showing things that never were nor could be.",
-    "author": "Fred Brooks",
-    "source": "<em>The Mythical Man-Month: Essays on Software Engineering</em>"
-  },
-  {
-    "quote": "Computer programs are the most intricate, delicately balanced and finely interwoven of all the products of human industry to date. They are machines with far more moving parts than any engine: the parts don't wear out, but they interact and rub up against one another in ways the programmers themselves cannot predict.",
-    "author": "James Gleick",
-    "source": "<em>What just happened: a chronicle from the information frontier</em>"
-  },
-  {
-    "quote": "Applications programming is a race between software engineers, who strive to produce idiot-proof programs, and the universe which strives to produce bigger idiots. So far the Universe is winning.",
-    "author": "Rick Cook",
-    "source": "<em>The Wizardry Compiled</em>"
+    "quote": "You‘ve got to start with the customer experience and work back toward the technology - not the other way around.",
+    "author": "Steve Jobs",
+    "source": "WWDC 1997"
   },
   {
     "quote": "The effective exploitation of his powers of abstraction must be regarded as one of the most vital activities of a competent programmer.",
@@ -50,26 +30,6 @@ var quotes = [
     "source": "'On research in structured programming'. <em>SIGPLAN Not.</em>, 10(10), pp.16-17"
   },
   {
-    "quote": "If I ask another professor what he teaches in the introductory programming course, whether he answers proudly \"Pascal\" or diffidently \"FORTRAN,\" I know that he is teaching a grammar, a set of semantic rules, and some finished algorithms, leaving the students to discover, on their own, some process of design.",
-    "author": "Robert Floyd",
-    "source": "'The Paradigms of Programming,' <em>Communications of the ACM</em> Vol 22 (8) p. 455-460"
-  },
-  {
-    "quote": "No matter how slick the demo is in rehearsal, when you do it in front of a live audience, the probability of a flawless presentation is inversely proportional to the number of people watching, raised to the power of the amount of money involved.",
-    "author": "Mark Gibbs",
-    "source": ""
-  },
-  {
-    "quote": "[This] reminds me of a quotation from somebody that, whenever he tried to explain the logical structure of a programming language to a programmer, it was like a cat trying to explain to a fish what it feels like to be wet.",
-    "author": "Saul Gorn",
-    "source": "Quoted by Julien Green in <em>Microprogramming, Emulators and Programming Languages</em>"
-  },
-  {
-    "quote": "There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies. And the other way is to make it so complicated that there are no obvious deficiencies.",
-    "author": "C.A.R. Hoare",
-    "source": ""
-  },
-  {
     "quote": "To me programming is more than an important practical art. It is also a gigantic undertaking in the foundations of knowledge.",
     "author": "Grace Hopper",
     "source": "Quoted in <em>Management and the Computer of the Future</em>"
@@ -80,29 +40,9 @@ var quotes = [
     "source": ""
   },
   {
-    "quote": "Most software today is very much like an Egyptian pyramid with millions of bricks piled on top of each other, with no structural integrity, but just done by brute force and thousands of slaves.",
-    "author": "Alan Kay",
-    "source": ""
-  },
-  {
     "quote": "Premature optimization is the root of all evil.",
     "author": "Donald Knuth",
     "source": "'Structured Programming with Goto Statements,' <em>Computing Surveys</em>"
-  },
-  {
-    "quote": "The most important thing in a programming language is the name. A language will not succeed without a good name. I have recently invented a very good name, and now I am looking for a suitable language.",
-    "author": "Donald Knuth",
-    "source": ""
-  },
-  {
-    "quote": "Present-day computers are designed primarily to solve preformulated problems or to process data according to predetermined procedures. The course of the computation may be conditional upon results obtained during the computation, but all the alternatives must be foreseen in advance. &#x2026 The requirement for preformulation or predetermination is sometimes no great disadvantage. It is often said that programming for a computing machine forces one to think clearly, that it disciplines the thought process. If the user can think his problem through in advance, symbiotic association with a computing machine is not necessary.",
-    "author": "J. C. R. Licklider",
-    "source": "<em>Man-Computer Symbiosis</em>"
-  },
-  {
-    "quote": "One in a million is next Tuesday.",
-    "author": "Gordon Letwin",
-    "source": ""
   },
   {
     "quote": "He who hasn't hacked assembly language as a youth has no heart. He who does as an adult has no brain.",
@@ -113,16 +53,6 @@ var quotes = [
     "quote": "Languages shape the way we think, or don't.",
     "author": "Erik Naggum",
     "source": ""
-  },
-  {
-    "quote": "Computer programming is tremendous fun. Like music, it is a skill that derives from an unknown blend of innate talent and constant practice. Like drawing, it can be shaped to a variety of ends &#x2013; commercial, artistic, and pure entertainment. Programmers have a well-deserved reputation for working long hours, but are rarely credited with being driven by creative fevers. Programmers talk about software development on weekends, vacations, and over meals not because they lack imagination, but because their imagination reveals worlds that others cannot see.",
-    "author": "Larry O'Brien and Bruce Eckel",
-    "source": "<em>Thinking in C#</em>"
-  },
-  {
-    "quote": "The best book on programming for the layman is <em>Alice in Wonderland</em>, but that's because it's the best book on anything for the layman.",
-    "author": "Alan Perlis",
-    "source": "'Epigrams on Programming'"
   },
   {
     "quote": "Computer Science is embarrassed by the computer.",
@@ -145,11 +75,6 @@ var quotes = [
     "source": "'Epigrams on Programming'"
   },
   {
-    "quote": "When someone says: \"I want a programming language in which I need only say what I wish done\", give him a lollipop.",
-    "author": "Alan J. Perlis",
-    "source": ""
-  },
-  {
     "quote": "Software and cathedrals are much the same &#x2013; first we build them, then we pray.",
     "author": "Sam Redwine",
     "source": ""
@@ -160,19 +85,9 @@ var quotes = [
     "source": ""
   },
   {
-    "quote": "A Netscape engineer who shan't be named once passed a pointer to JavaScript, stored it as a string and later passed it back to C, killing 30.",
-    "author": "Blake Ros",
-    "source": ""
-  },
-  {
     "quote": "Real Programmers always confuse Christmas and Halloween because Oct31 == Dec25.",
     "author": "Andrew Rutherford",
     "source": ""
-  },
-  {
-    "quote": "Anyone even peripherally involved with computers agrees that object-oriented programming (OOP) is the wave of the future. Maybe one in 50 of them has actually tried to use OOP &#x2013; which has a lot to do with its popularity.",
-    "author": "Steve Steinberg",
-    "source": "'Hype List', <em>Wired</em>"
   },
   {
     "quote": "Don't get suckered in by the comments &#x2026 they can be terribly misleading.",
@@ -193,11 +108,6 @@ var quotes = [
     "quote": "Weinberg's Second Law: If builders built buildings the way programmers wrote programs, then the first woodpecker that came along would destroy civilization.",
     "author": "Gerald Weinberg",
     "source": "Cited by Murali Chemuturi in <em>Mastering Software Quality Assurance: Best Practices, Tools and Technique for Software Developers</em>"
-  },
-  {
-    "quote": "Asking for efficiency and adaptability in the same program is like asking for a beautiful and modest wife. Although beauty and modesty have been known to occur in the same woman, we'll probably have to settle for one or the other. At least that's better than neither.",
-    "author": "Gerald M. Weinberg",
-    "source": "<em>The Psychology of Computer Programming</em>"
   },
   {
     "quote": "The main activity of programming is not the origination of new independent programs, but in the integration, modification, and explanation of existing ones.",
